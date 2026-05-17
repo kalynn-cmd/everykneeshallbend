38fc434cefe666ae6e56cb04959a76b0188ad424
kalynn-cmd <kalynn@jacqueline-brown.com>
2026-05-17 14:04:16 -0400

    Add files via upload

--- sendmail.php
+++ sendmail.php
@@ -1,10 +1,50 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-<body>
-</body>
-</html>
+    $to = "everykneeshallbend@gmail.com";
+    $subject = "New Altar Rail Grant Request";
+
+    $parishName = htmlspecialchars($_POST["parishName"]);
+    $parishAddress = htmlspecialchars($_POST["parishAddress"]);
+    $pastorName = htmlspecialchars($_POST["pastorName"]);
+    $dioceseName = htmlspecialchars($_POST["dioceseName"]);
+    $bishopName = htmlspecialchars($_POST["bishopName"]);
+    $projectCost = htmlspecialchars($_POST["projectCost"]);
+    $fullAmountRequest = htmlspecialchars($_POST["fullAmountRequest"]);
+    $otherFunding = htmlspecialchars($_POST["otherFunding"]);
+    $contactPerson = htmlspecialchars($_POST["contactPerson"]);
+    $contactEmail = htmlspecialchars($_POST["contactEmail"]);
+    $contactPhone = htmlspecialchars($_POST["contactPhone"]);
+
+    $message = "
+New Altar Rail Grant Request
+
+Name of Parish: $parishName
+Address of Parish: $parishAddress
+Name of Pastor: $pastorName
+Name of Diocese: $dioceseName
+Name of Bishop: $bishopName
+Expected Project Cost: $projectCost
+Asking for Full Amount: $fullAmountRequest
+
+Other Funding Information:
+$otherFunding
+
+Contact Person: $contactPerson
+Contact Email: $contactEmail
+Contact Phone: $contactPhone
+";
+
+    $headers = "From: $contactEmail\r\n";
+    $headers .= "Reply-To: $contactEmail\r\n";
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+
+} else {
+    echo "Invalid request.";
+}
+?>
